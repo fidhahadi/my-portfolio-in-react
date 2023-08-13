1015c403e4b9ff256c5af111d15e6425a15050a1
Fidha Rafeeque <fidharafeequen@gmail.com>
2023-08-13 21:30:01 +0400

    typed is implemented

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -19,7 +19,7 @@ function Contact() {
             </a>
           </div><br />
           <div>
-            <a href="#" class="block max-w-sm p-6 bg-black-100 border border-gray-200 rounded-lg shadow hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <a href="#" class="block max-w-sm p-6 bg-black border border-gray-200 rounded-lg shadow hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-400 dark:text-white">Location</h5>
                 <p class="font-normal text-gray-400 dark:text-gray-400">Al Nahda <br /></p>
             </a>
--- src/components/mainpage/Mainpage.js
+++ src/components/mainpage/Mainpage.js
@@ -2,15 +2,34 @@ import React from 'react'
 import './Mainpage.css';
 import {ButtonGroup, Button} from '@chakra-ui/react'
 import CV from "./Fidha_Rafeeque_N_Resume.pdf";
-
+import Typed from 'typed.js';
+import { useEffect, useRef } from 'react';
 
 function Mainpage() {
+
+  //create ref element
+  const el = useRef(null);
+
+  useEffect(()=> {
+    const typed = new Typed(el.current, {
+      strings: ["Web Developer", "Full Stack Developer", "Application Developer"],
+      startDelay: 200,
+      typeSpeed: 150,
+      backSpeed: 60,
+      backDelay: 150,
+      smartBackspace: true,
+      showCursor: false,
+      loop: true,
+
+    })
+  })
 
   return (
     <div className='container-mainpage'>
       <div className='sub-container-mainpage'>
       <p className='text-6xl'>Hi, I am Fidha Rafeeque N</p><br />
-      <p className='text-5xl'>I am a Full Stack Developer</p><br />
+      <p className='text-5xl'>I am a <span ref={el}></span>
+      </p><br />
       
 
       <ButtonGroup spacing='2'>
