6c6de2e94f29d18d5d983cb84520234cde97e898
Fidha Rafeeque <fidharafeequen@gmail.com>
2023-08-13 21:01:35 +0400

    io

--- src/components/mainpage/Mainpage.js
+++ src/components/mainpage/Mainpage.js
@@ -5,11 +5,14 @@ import CV from "./Fidha_Rafeeque_N_Resume.pdf";
 
 
 function Mainpage() {
+
   return (
     <div className='container-mainpage'>
       <div className='sub-container-mainpage'>
       <p className='text-6xl'>Hi, I am Fidha Rafeeque N</p><br />
       <p className='text-5xl'>I am a Full Stack Developer</p><br />
+      
+
       <ButtonGroup spacing='2'>
             <Button variant='solid' colorScheme='blue'>
               Hire Me
