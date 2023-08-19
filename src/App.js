4c5db57ee3036d53aa7cad094a36d265903005b6
Fidha Rafeeque <fidharafeequen@gmail.com>
2023-08-19 12:32:40 +0400

    frontend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import './App.css';
-import AboutMe from './components/aboutme/AboutMe';
-import Contact from './components/contact/Contact';
-import Mainpage from './components/mainpage/Mainpage';
-import Portfolio from './components/portfolio/Portfolio';
-import Resume from './components/resume/Resume';
-
-
-function App() {
-  return (
-    <div>
-      
-      <Mainpage />
-      <AboutMe />
-      <Resume />
-      <Portfolio />
-      <Contact />
-    </div>
-  );
+import React, { Component } from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import Testimonials from  './components/Testimonials';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
+import resumeData from './resumeData';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header resumeData={resumeData}/>
+        <About resumeData={resumeData}/>
+        <Resume resumeData={resumeData}/>
+        <Portfolio resumeData={resumeData}/>
+        <Testimonials resumeData={resumeData}/>
+        <ContactUs resumeData={resumeData}/>
+        <Footer resumeData={resumeData}/>
+      </div>
+    );
+  }
 }
 
 export default App;
